ccfde14a870bc3f14709deefbbe19cf08145637f
Noah Lachine <noah.lachine@ontariotechu.net>
2020-09-10 10:01:04 -0400

    [MOD] edited urls to direct first 3 categories

--- AquaBlue/src/core-components/LandingPage/TopCategories.js
+++ AquaBlue/src/core-components/LandingPage/TopCategories.js
@@ -6,14 +6,14 @@ import './TopCategories.scss';
 
 import ImgOne from '../../assets/images/ThankYou.png';
 import ImgOneHover from '../../assets/images/ThankYou_Active.png';
-import ImgTwo from '../../assets/images/Baby.png';
-import ImgTwoHover from '../../assets/images/Baby_Active.png';
-import ImgThre from '../../assets/images/Christmas.png';
-import ImgThreeHover from '../../assets/images/Christmas_Active.png';
+import ImgTwo from '../../assets/images/Sympathy.png';
+import ImgTwoHover from '../../assets/images/Sympathy_Active.png';
+import ImgThre from '../../assets/images/Friendship.png';
+import ImgThreeHover from '../../assets/images/Friendship_Active.png';
 import ImgFour from '../../assets/images/Easter.png';
 import ImgFourHover from '../../assets/images/Easter_Active.png';
-import ImgFive from '../../assets/images/Friendship.png';
-import ImgFiveHover from '../../assets/images/Friendship_Active.png';
+import ImgFive from '../../assets/images/Christmas.png';
+import ImgFiveHover from '../../assets/images/Christmas_Active.png';
 import ImgSix from '../../assets/images/Corporate.png';
 import ImgSixHover from '../../assets/images/Corporate_Active.png';
 
@@ -32,7 +32,7 @@ export default function TopCategories() {
             <Box className="topcategories-container">
                 <Grid className="topcategories-container" container direction="row" justify="center" alignItems="center" spacing={10}>
                     <Grid item xs={6} sm={3}>
-                        <ButtonBase className="grid-button-base">
+                        <ButtonBase className="grid-button-base" href="http://mk-xmpie.mk.local/ustorethemes/FALVersionOne/7/en-US/category/33/Thank-You/">
                             <div className="icon-button-container">
                                 <HoverImage className="icon" src={ImgOne} hoverSrc={ImgOneHover}></HoverImage>
                                 {/* <TopCategoriesLabel>Birthday</TopCategoriesLabel> */}
@@ -40,7 +40,7 @@ export default function TopCategories() {
                         </ButtonBase>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <ButtonBase className="grid-button-base">
+                        <ButtonBase className="grid-button-base" href="http://mk-xmpie.mk.local/ustorethemes/FALVersionOne/7/en-US/category/32/Sympathy/">
                             <div className="icon-button-container">
                                 <HoverImage className="icon" src={ImgTwo} hoverSrc={ImgTwoHover}></HoverImage>
                                 {/* <TopCategoriesLabel>New Born</TopCategoriesLabel> */}
@@ -48,7 +48,7 @@ export default function TopCategories() {
                         </ButtonBase>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <ButtonBase className="grid-button-base">
+                        <ButtonBase className="grid-button-base" href="http://mk-xmpie.mk.local/ustorethemes/FALVersionOne/7/en-US/category/30/Friendship/">
                             <div className="icon-button-container">
                                 <HoverImage className="icon" src={ImgThre} hoverSrc={ImgThreeHover}></HoverImage>
                                 {/* <TopCategoriesLabel>Christmas</TopCategoriesLabel> */}
