f678e9bd8aecc9fb70290d09541b58907dea9c52
Noah Lachine <noah.lachine@ontariotechu.net>
2020-09-15 09:43:55 -0400

    [MOD] FAQ font change

--- AquaBlue/src/core-components/FAQ/SimpleAccordion.js
+++ AquaBlue/src/core-components/FAQ/SimpleAccordion.js
@@ -5,15 +5,20 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width: '30%',
+        width: '40%',
         padding: '50px 0px',
         margin: 'auto',
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
+        fontFamily: 'Montserrat, Arial, sans-serif',
         fontWeight: theme.typography.fontWeightRegular,
     },
+    body: {
+        fontFamily: 'Montserrat, Arial, sans-serif',
+    },
 }));
+
 
 export default function SimpleAccordion() {
     const classes = useStyles();
@@ -30,7 +35,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 1</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                    <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
@@ -46,7 +51,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 2</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
@@ -62,7 +67,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 3</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
@@ -79,7 +84,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 4</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
@@ -96,7 +101,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 5</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
@@ -113,7 +118,7 @@ export default function SimpleAccordion() {
                     <Typography className={classes.heading}>Accordion 6</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                <Typography className={classes.body}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
                         sit amet blandit leo lobortis eget.
                     </Typography>
